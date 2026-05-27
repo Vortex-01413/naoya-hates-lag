d7e3e328399c8073916ca5425f703fbd5afa4f79
Vortex-01413 <asojoiretomiwa@gmail.com>
2026-05-27 15:32:37 +0100

    Fix: Cleaned up package and import typos in WorldRendererMixin

--- src/main/java/com/naoya/lag/mixin/render/WorldRendererMixin.java
+++ src/main/java/com/naoya/lag/mixin/render/WorldRendererMixin.java
@@ -1,11 +1,11 @@
-kpackage com.naoya.lag.mixin.render;
+package com.naoya.lag.mixin.render;
 
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import org.joml/Matrix4f;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
